8bec4acab18c000cedc9f8cfa39e798210f74ddd
good0161 <anderwa.goodspeed@ou.edu>
2023-02-28 19:45:30 -0600

    Created TripPoint Class, its construtor& accessors

---
+++ src/TripPoint.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 
+ * @author good0161
+ * @version 1.0.0
+ * 
+ */
+
+public class TripPoint {
+    
+    private double lat;
+    private double lon;
+    private int time;
+    private static ArrayList<TripPoint> trip;
+
+    /**
+     * 
+     * @param time
+     * @param lat
+     * @param lon
+     * Initilizes varibles of a new TripPoint object
+     */
+    public TripPoint(int time, double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+        this.time = time;
+    }
+
+    //accessor methods
+
+    /**
+     * 
+     * @return time
+     * gets the time
+     */
+    public int getTime(){
+        return time;
+    }
+    /**
+     * 
+     * @return lat
+     * gets Latitude
+     */
+    public double getLat(){
+        return lat;
+    }
+    /**
+     * 
+     * @return lon
+     * gets Longitude
+     */
+    public double getLon(){
+        return lon;
+    }
+    /**
+     * 
+     * @return copy of trip
+     * Makes a copy of the trips ArrayList to prevent mutation and returns it
+     */
+    public static ArrayList<TripPoint> getTrip(){
+        ArrayList<TripPoint> copy = new ArrayList<>(trip);
+        return copy;
+    }
+
+
+}
